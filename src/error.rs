fe96763c87030da866576b86e4ae53f14ee2faed
René Rössler <rene@freshx.de>
2026-06-11 11:53:47 +0200

    better error messages, update dependencies

--- src/error.rs
+++ src/error.rs
@@ -2,9 +2,9 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Could not parse the url")]
+    #[error("Could not parse the url: {0:?}")]
     InvalidUrl(#[from] url::ParseError),
-    #[error("Request failed")]
+    #[error("Request failed: {0:?}")]
     RequestFailed(#[from] reqwest::Error),
     #[error("Server answered with non-ok status: {status}. body: {body}")]
     InvalidResponse {
